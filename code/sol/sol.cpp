7674ec074b8087f5ce7db00d298ebdcb7518f479
Caelum Pirata <caelumpirata@gmail.com>
2025-03-11 08:25:55 +0530

    specific profile added for cp

--- code/sol/sol.cpp
+++ code/sol/sol.cpp
@@ -1,3 +1,7 @@
+/**
+ *    author:  caelumpirata
+ *    created: 11.03.2025 08:22:55
+**/
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -5,6 +9,6 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  
+
   return 0;
 }
