22cd9a9a43f45c731cd78b2f42be744456205b1c
Caelum Pirata <85424262+caelumpirata@users.noreply.github.com>
2025-03-17 01:07:58 +0530

    c++ 20 used

--- sol.bat
+++ sol.bat
@@ -1,4 +1,4 @@
 @echo off
-g++ sol.cpp -o sol.exe -O2 -Wall -Wl,--stack=536870912 -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG
+g++ sol.cpp -o sol.exe -O2 -Wall -Wl,--stack=536870912 -std=c++20 -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG
 sol.exe
 del sol.exe
