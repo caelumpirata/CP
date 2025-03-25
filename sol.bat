c9c60f4ee47cccd08ce3470e71579f717fe2728e
Caelum Pirata <85424262+caelumpirata@users.noreply.github.com>
2025-03-25 23:21:18 +0530

    :)

--- sol.bat
+++ sol.bat
@@ -1,4 +1,5 @@
 @echo off
-g++ sol.cpp -o sol -O2 -Wall -Wextra -std=c++20 -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -fmax-errors=1
-sol.exe
-del sol.exe
+g++ sol.cpp -o soln.exe -O2 -Wall -Wextra -std=c++20 -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -fmax-errors=1
+soln.exe && pause
+del soln.exe
+exit
